c74774c2bb0f627e62f17f3755591a5aebb07a15
LoboAnimae <30876166+LoboAnimae@users.noreply.github.com>
2018-02-09 23:08:34 -0600

    Changed insertion Class

--- Documents/NetBeansProjects/Insertion/src/insertion/Insertion.java
+++ Documents/NetBeansProjects/Insertion/src/insertion/Insertion.java
@@ -14,59 +14,58 @@ import java.io.FileReader;
  */
 public class Insertion {
 
-
-    public static void main(String []args){
+    public static void main(String[] args) {
         BufferedReader bf = null;
         String direccion = "C:\\Users\\Andres\\Documents\\NetBeansProjects\\Hoja3\\numeros.txt";
         String texto = "";
-        int i = 0;      
-        
-        
+        int i = 0;
+
         //lee el archivo de texto
-        try{
+        try {
             bf = new BufferedReader(new FileReader(direccion));
             String temporal = "";
             String bfRead;
-            while((bfRead = bf.readLine()) !=null){
+            while ((bfRead = bf.readLine()) != null) {
                 temporal = temporal + bfRead;
             }
-            
+
             texto = temporal;
-            
-        }catch(Exception e){
+
+        } catch (Exception e) {
             //Error por si no ecuentra el archivo
             System.out.println("No se encontro archivo");
-            
+
         }
-        
+
         //Separa el
         String parts[] = texto.split(" ");
-        
+
         //convertir el array de String a integer
         int array[] = new int[parts.length];
-        for(int j = 0; j < parts.length; j++){
-            
-            array[i] = Integer.parseInt(parts[i]); 
-            
+        for (int j = 0; j < parts.length; j++) {
+
+            array[i] = Integer.parseInt(parts[i]);
+
         }
         //System.out.println(parts[1]);
-        
+
         //sort va aca
         long startTime = System.currentTimeMillis();
         // ----> aca
-    
-}
-    public static int[] insertion(int[] input){
-           int temporal;
-           for (int i = 1; i < input.length; i++){
-               for(int j = i; j > 0 ; j--){
-                   if(input[j] < input[j-1]){
-                       temporal = input[j];
-                       input[j] = input[j-1];
-                       input[j-1] =temporal;
-                   }
-               }
-           }
-           return input;
+
+    }
+
+    public static void insertionSort(int[] a, String direction) {
+        int input, output;
+        for (output = 1; output < a.length; output++) {
+            int temporal = a[output];
+            input = output;
+            while ((input > 0 && a[input - 1] >= temporal && direction.equalsIgnoreCase("A")) || (input > 0) && a[input - 1] <= temporal && direction.equalsIgnoreCase("D")) {
+                a[input] = a[input - 1];
+                --input;
+            }
+            a[input] = temporal;
+
         }
+    }
 }
